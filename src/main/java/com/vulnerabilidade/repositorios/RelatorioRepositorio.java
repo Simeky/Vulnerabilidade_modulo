25e99a1cb5fa505e58563cafe183644f2c6ea702
Guilherme Carsten Rayser <92762741+Simeky@users.noreply.github.com>
2023-11-29 10:52:28 -0300

    Delete src/main/java/com/vulnerabilidade/repositorios/RelatorioRepositorio.java

--- src/main/java/com/vulnerabilidade/repositorios/RelatorioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.vulnerabilidade.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vulnerabilidade.classes.Relatorio;
-
-public interface RelatorioRepositorio extends JpaRepository<Relatorio, Long>{
-    
-}
